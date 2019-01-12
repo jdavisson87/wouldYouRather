ac389667dea0db01ac2ca0131a5d1ee96042232e
Kittaka <jdavisson87@gmail.com>
2019-01-12 16:50:47 -0600

    Creating default files
    Creating default files to start the project

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div>Loadout</div>
+      </div>
+    );
+  }
+}
+
+export default App;
