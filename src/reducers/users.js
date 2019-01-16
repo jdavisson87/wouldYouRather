1a34974dcd4781fe92cb0385c6bdf0618b731ed9
Kittaka <jdavisson87@gmail.com>
2019-01-16 16:01:25 -0600

    User to Users
    Changed function name from user to users

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,6 +1,6 @@
 import { RECEIVE_USERS/*, ADD_USER*/ } from '../actions/users'
 
-export default function user (state= {}, action) {
+export default function users (state= {}, action) {
   switch(action.type) {
     case RECEIVE_USERS :
       return {
