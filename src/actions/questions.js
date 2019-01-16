d09c40de25c9e48093d291f03a4f795961a2cf98
Kittaka <jdavisson87@gmail.com>
2019-01-16 15:20:38 -0600

    Question Action and Reducer
    Created question action and reducer and updated the index in reducer
    to include the questions reducer with the combineReducer from redux

---
+++ src/actions/questions.js
@@ -0,0 +1,16 @@
+export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
+export const ADD_QUESTION = 'ADD_QUESTION'
+
+function addQuestion(question) {
+  return {
+    type: ADD_QUESTION,
+    question
+  }
+}
+
+function receiveQuestion(question {
+  return {
+    type: RECEIVE_QUESTIONS,
+    questions
+  }
+})
