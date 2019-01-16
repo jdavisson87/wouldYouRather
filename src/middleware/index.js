154ed400f55fe2fda58b4b6b1e58262b4c1aeba2
Kittaka <jdavisson87@gmail.com>
2019-01-16 16:52:58 -0600

    Spelling error
    Corrected the spelling of middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,6 +1,6 @@
 import thunk from 'redux-thunk';
 import logger from './logger';
-import { applyMiddlware } from 'redux';
+import { applyMiddleware } from 'redux';
 
 export default applyMiddleware(
   thunk,
