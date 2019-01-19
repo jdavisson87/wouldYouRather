24a843a6eeda63bf8849fbc6c797696b2903c882
Kittaka <jdavisson87@gmail.com>
2019-01-18 18:24:50 -0600

    Commented out Setting the Auth User
    Commented out the authedUser and the dispatch function that initially
    sets the authedUser state.

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -3,7 +3,7 @@ import { receiveUsers } from '../actions/users'
 import { receiveQuestions } from '../actions/questions'
 import { setAuthedUser } from '../actions/authedUser'
 
-const AUTHED_ID = 'sarahedo'
+// const AUTHED_ID = 'sarahedo'
 
 export function handleInitialData() {
   return (dispatch) => {
@@ -11,7 +11,7 @@ export function handleInitialData() {
       .then(({ users, questions })=> {
         dispatch(receiveUsers(users))
         dispatch(receiveQuestions(questions))
-        dispatch(setAuthedUser(AUTHED_ID)) //will change once when are selecting the auth user
+        //dispatch(setAuthedUser(AUTHED_ID)) //will change once when are selecting the auth user
       })
   }
 }
