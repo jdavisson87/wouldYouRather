53ebec6d102228f289acca84dba95005d7c68ce3
Kittaka <jdavisson87@gmail.com>
2019-01-18 18:22:00 -0600

    Login Component
    Created a Login Component that has access to the users state and is
    able to update the authedUser state by using the
    dispatch(setAuthedUser(id))

---
+++ src/components/Login.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { setAuthedUser } from '../actions/authedUser'
+
+class Login extends Component {
+
+  handleSubmit = id => {
+    const {dispatch} = this.props;
+    dispatch(setAuthedUser(id))
+  }
+
+  render() {
+
+    const { users, userIds, authedUser } = this.props
+    console.log({authedUser})
+    return (
+      <div>
+        {users.length !== 0 && <h3 className='center'>Click on a User to Login:</h3>}
+        <ul className='login-users'>
+          {Object.keys(users).map(id =>(
+            <li key={id} className = 'user' onClick={(e)=>this.handleSubmit(id)}>
+              <img
+                src={users[id].avatarURL}
+                alt={`Avatar of ${users[id].name}`}
+                className='avatar'
+              />
+              <p>{users[id].name}</p>
+
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps({ users, authedUser }) {
+  return {
+    userIds: Object.keys(users),
+    users,
+    authedUser,
+  }
+}
+
+export default connect(mapStateToProps)(Login)
