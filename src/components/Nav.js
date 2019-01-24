f874f0bb77c69562a05d00cdd1586f47ee879f28
Kittaka <jdavisson87@gmail.com>
2019-01-23 18:10:39 -0600

    Nav Component with Authed User
    Added to the Nav Component the authedUser and the authedUser avatar, so
    after the user logs in, they will have their avatar showing

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-export default function Nav () {
-  return (
-    <nav className='nav'>
-      <ul className='nav-ul'>
-        <li>
-          <NavLink to='/' exact activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  )
+class Nav extends Component {
+  render() {
+    const { authedUser, users } = this.props
+    return (
+      <div>
+        <nav className='nav'>
+          <ul className='nav-ul col-md-8'>
+            <li>
+              <NavLink to='/' exact activeClassName='active'>
+                Home
+              </NavLink>
+            </li>
+          </ul>
+          {authedUser !== null && (<span className='col-md-3 text-right'>
+            <img
+              src={users[authedUser].avatarURL}
+              alt={`Avatar of ${users[authedUser].name}`}
+              className='avatar'
+            />
+            <span>{authedUser}</span>
+          </span>)}
+        </nav>
+      </div>
+    )
+  }
 }
+
+function mapStateToProps({ authedUser,users }){
+  return {
+    authedUser,
+    users
+  }
+}
+
+export default connect(mapStateToProps)(Nav)
