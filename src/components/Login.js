2850b86b6aa1237f643f5ad02d6193fd315235c0
Kittaka <jdavisson87@gmail.com>
2019-01-31 14:32:52 -0600

    Login goes to Dashboard
    Added Link from react-router-dom to each user so when clicked, the
    Dashboard component will load as the default option

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { setAuthedUser } from '../actions/authedUser'
+import { Link } from 'react-router-dom'
 
 class Login extends Component {
 
@@ -18,15 +19,16 @@ class Login extends Component {
         {users.length !== 0 && <h3 className='center'>Click on a User to Login:</h3>}
         <ul className='login-users'>
           {Object.keys(users).map(id =>(
-            <li key={id} className = 'user' onClick={(e)=>this.handleSubmit(id)}>
-              <img
-                src={users[id].avatarURL}
-                alt={`Avatar of ${users[id].name}`}
-                className='avatar'
-              />
-              <p>{users[id].name}</p>
-
-            </li>
+            <Link to='/' key={id}>
+              <li className = 'user' onClick={(e)=>this.handleSubmit(id)}>
+                <img
+                  src={users[id].avatarURL}
+                  alt={`Avatar of ${users[id].name}`}
+                  className='avatar'
+                />
+                <p>{users[id].name}</p>
+              </li>
+            </Link>
           ))}
         </ul>
       </div>
