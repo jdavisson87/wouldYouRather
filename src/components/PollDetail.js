c5ae5d7fd3129ad4fe463332dcfdc169ea24aaa4
Kittaka <jdavisson87@gmail.com>
2019-02-01 17:58:52 -0600

    Removed commented out code in PollDetail
    Removed the commented out code in PollDetail for the unanswered
    component.  Now that the component is connected and is in a separate
    file, the code is not neccessary.

--- src/components/PollDetail.js
+++ src/components/PollDetail.js
@@ -15,35 +15,6 @@ function PollDetail(props) {
     }else{
       return(
         <Unanswered id={id}/>
-        // <li key={props.question.id}>
-        //   <h3 className='text-center'>Would You Rather:</h3>
-        //   <div className='question-info'>
-        //     <span>Author: {users[question.author].name}
-        //       <img
-        //         src={users[question.author].avatarURL}
-        //         alt={`Avatar of ${users[question.author].name}`}
-        //         className='avatar'
-        //       />
-        //     </span>
-        //     <span className='pull-right'>{formatDate(question.timestamp)}</span>
-        //   </div>
-        //   <div className='option'>
-        //     <h4>
-        //       Option One: {props.question.optionOne.text}
-        //     </h4>
-        //     <button className='btn btn-info pull-right' onClick={e =>{
-        //       dispatch(handleAddQuestionAnswer(id, 'optionOne'))
-        //     }}>Vote</button>
-        //   </div>
-        //   <div className='option'>
-        //     <h4>
-        //       Option Two: {props.question.optionTwo.text}
-        //     </h4>
-        //     <button className='btn btn-info pull-right' onClick={e =>{
-        //       dispatch(handleAddQuestionAnswer(id, 'optionTwo'))
-        //     }}>Vote</button>
-        //   </div>
-        // </li>
       )
     }
   }
